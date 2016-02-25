e8f016ed8f4cf91c37409b5c39413eea58c83774
Mike Zueff <mike.zueff@gmail.com>
2016-02-26 02:27:52 +0300

    Implemented single dex invokation from awesome WM.

--- .config/awesome/stunning.lua
+++ .config/awesome/stunning.lua
@@ -1,16 +1,29 @@
 local awful = require("awful")
 
+local awesome_dex_lock_file = "dex_have_been_executed"
+local awesome_run_time_directory = "/tmp/run_time/awesome-related"
+
 function execute_once(process_name, argv, launcher)
+  if process_name == "dex" and awful.util.file_readable(awesome_run_time_directory .. "/" .. awesome_dex_lock_file) then
+    return
+  end
+
   if not launcher then
     launcher = process_name
   end
 
   if argv then
-    awful.util.spawn_with_shell("pgrep --euid ${USER} --exact '" .. launcher .. " " .. argv .. "' --full || (" .. process_name .. " " .. argv .. " &)")
+    awful.util.spawn_with_shell("pgrep --euid ${USER} --exact '" .. process_name .. " " .. argv .. "' --full || (" .. launcher .. " " .. argv .. " &)")
   else
-    awful.util.spawn_with_shell("pgrep --euid ${USER} --exact '" .. launcher .. "' --full || (" .. process_name .. " &)")
+    awful.util.spawn_with_shell("pgrep --euid ${USER} --exact '" .. process_name .. "' --full || (" .. launcher .. " &)")
+  end
+
+  if process_name == "dex" then
+    awful.util.spawn_with_shell("mkdir --parents " .. awesome_run_time_directory)
+    awful.util.spawn_with_shell("touch " .. awesome_run_time_directory .. "/" .. awesome_dex_lock_file)
   end
 end
 
 awful.util.spawn_with_shell("nitrogen --restore")
+execute_once("dex", "--autostart")
 execute_once("goldendict")
