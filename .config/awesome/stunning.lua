b4f0bc3594f086fc282895fc0dd385354588c88c
Mike Zueff <mike.zueff@gmail.com>
2016-02-26 01:26:14 +0300

    Added dex execution to awesome WM config.

--- .config/awesome/stunning.lua
+++ .config/awesome/stunning.lua
@@ -1,3 +1,16 @@
 local awful = require("awful")
 
+function execute_once(process_name, argv, launcher)
+  if not launcher then
+    launcher = process_name
+  end
+
+  if argv then
+    awful.util.spawn_with_shell("pgrep --euid ${USER} --exact '" .. launcher .. " " .. argv .. "' --full || (" .. process_name .. " " .. argv .. " &)")
+  else
+    awful.util.spawn_with_shell("pgrep --euid ${USER} --exact '" .. launcher .. "' --full || (" .. process_name .. " &)")
+  end
+end
+
 awful.util.spawn_with_shell("nitrogen --restore")
+execute_once("goldendict")
