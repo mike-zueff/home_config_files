93d686f34ee7dec3ac9622ee7b7ba6995a3f5f1a
Mike Zueff <mike.zueff@gmail.com>
2016-03-12 05:47:58 +0300

    Fixed smartphone back up script.

--- .config/rc_files/smartphone.sh
+++ .config/rc_files/smartphone.sh
@@ -5,23 +5,31 @@ function _rc_smartphone_back_up
   _RC_CURRENT_DATE_STAMP=`date +%Y_%m_%d_%H_%M_%S`
   _RC_SMARTPHONE_BACKUPS_DIRECTORY="/private/backups/xperia_z3+_dual"
 
-  _rc_smartphone_umount
-  sleep 5
-  adb kill-server
-  sleep 5
-
   if [[ ! -d ${_RC_SMARTPHONE_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP} ]]
   then
     mkdir ${_RC_SMARTPHONE_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP}
+
+    _rc_smartphone_umount
+    sleep 5
+    adb kill-server
+    sleep 5
+
     _rc_smartphone_mount
+    sleep 5
+    tar --create --directory ~/mnt/xperia_z3+_dual "Internal storage" | xz --best > ${_RC_SMARTPHONE_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP}/internal_storage.tar.xz
+
+    _rc_smartphone_umount
     sleep 5
     adb devices
     sleep 5
-    tar --create --directory ~/mnt/xperia_z3+_dual "Internal storage" | xz --best > ${_RC_SMARTPHONE_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP}/internal_storage.tar.xz
-    notify-send "Please look at your smartphone."
     adb backup -f ${_RC_SMARTPHONE_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP}/android.ab -apk -obb -noshared -all -system
+
+    sleep 5
     _rc_smartphone_umount
+    sleep 5
     adb kill-server
+    sleep 5
+
     xz --best ${_RC_SMARTPHONE_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP}/android.ab
     xzcat ${_RC_SMARTPHONE_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP}/android.ab.xz | dd ibs=24 skip=1 | openssl zlib -d | xz --best > ${_RC_SMARTPHONE_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP}/android.tar.xz
 
