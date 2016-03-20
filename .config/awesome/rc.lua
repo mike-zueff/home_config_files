bac71b0016118c081585b4a5c2082cfade38c7f5
Mike Zueff <mike.zueff@gmail.com>
2016-03-21 02:17:29 +0300

    Added 2 more awesome WM tags, etc.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -83,7 +83,7 @@ end
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ " α", " β", " γ" }, s, layouts[6])
+    tags[s] = awful.tag({ " α", " β", " γ", " δ", " ε" }, s, layouts[6])
 end
 -- }}}
 
