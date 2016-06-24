5c7fb4a14a24f6720bafa88f40dbb8867c347273
Mike Zueff <mike.zueff@gmail.com>
2016-06-24 10:43:07 +0300

    Fixed VIM cleaner script.

--- .config/rc_files/vim.sh
+++ .config/rc_files/vim.sh
@@ -1,6 +1,6 @@
 function _rc_vim_reinstall
 {
-  rm --force --recursive ~/.cache/ctrlp ~/.cargo ~/.npm ~/.pylint.d ~/.vim \
-      ~/.viminfo ~/.viminfo.tmp ~/.viminfz.tmp ~/.vimtags
+  rm --force --recursive ~/.cache/ctrlp ~/.cache/jedi ~/.cargo ~/.npm \
+      ~/.pylint.d ~/.vim ~/.viminfo ~/.viminfo.tmp ~/.viminfz.tmp ~/.vimtags
   vim
 }
