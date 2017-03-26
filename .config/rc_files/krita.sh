0e78af3971b98220314209ae753582873858276b
Mike Zueff <mike.zueff@gmail.com>
2017-03-26 17:14:36 +0300

    Updated krita config files removal script.

--- .config/rc_files/krita.sh
+++ .config/rc_files/krita.sh
@@ -1,6 +1,6 @@
 function _rc_krita_clean_up
 {
-  rm --force --recursive ~/.kde4/share/apps/krita ~/.kde4/share/config/kritarc
+  rm --force --recursive ~/.config/kritarc ~/.local/share/krita
 
   find ~ -iname "*krita*" -not -path "${HOME}/.config/rc_files/*" -not -path \
       "${HOME}/.vim/*"
