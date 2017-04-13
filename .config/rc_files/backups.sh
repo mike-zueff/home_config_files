8ea94d850c847a95d2adfbb8d435b96b5a14e2c1
Mike Zueff <mike.zueff@gmail.com>
2017-04-13 23:42:45 +0300

    Fixed regex.

--- .config/rc_files/backups.sh
+++ .config/rc_files/backups.sh
@@ -4,8 +4,9 @@ function _util_clear_old_backups
   then
     CURRENT_YEAR=$(date +%Y)
     PREVIOUS_YEAR=$((CURRENT_YEAR - 1))
+    R=${1//+/\\+}
 
     find "${1}" -maxdepth 1 -mindepth 1 ! -type d -exec rm --force --recursive {} +
-    find "${1}" -maxdepth 1 -mindepth 1 ! -regex "^${1}/${CURRENT_YEAR}.*" ! -regex "^${1}/${PREVIOUS_YEAR}.*" -exec rm --force --recursive {} +
+    find "${1}" -maxdepth 1 -mindepth 1 ! -regex "^${R}/${CURRENT_YEAR}.*" ! -regex "^${R}/${PREVIOUS_YEAR}.*" -exec rm --force --recursive {} +
   fi
 }
