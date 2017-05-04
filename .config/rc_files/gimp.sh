31800b9659e3ccdd5743d7cb48193052b2c3ee97
Mike Zueff <mike.zueff@gmail.com>
2017-05-04 20:55:08 +0300

    Fixed GIMP cleaner.

--- .config/rc_files/gimp.sh
+++ .config/rc_files/gimp.sh
@@ -3,6 +3,6 @@ function _rc_gimp_clean_up
   rm --force --recursive ~/.cache/gegl-0.3 ~/.config/GIMP \
       ~/.local/share/gegl-0.3
 
-  find ~ -iname "*gegl*" -or -iname "*gimp*" -not -path \
-      "${HOME}/.config/rc_files/*" -not -path "${HOME}/.vim/*"
+  find ~ -iname "*gegl*" -or -iname "*gimp*" ! -path "${HOME}/.cargo/*" ! \
+      -path "${HOME}/.config/rc_files/*" ! -path "${HOME}/.vim/*"
 }
