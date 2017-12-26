e95d549c8cf594e405a3bd148ea9061a3264ec33
Mike Zueff <mike.zueff@gmail.com>
2017-12-26 11:45:56 +0300

    Fixed GIMP script.

--- .config/rc_files/gimp.sh
+++ .config/rc_files/gimp.sh
@@ -1,8 +1,9 @@
 function _rc_gimp_clean_up
 {
-  rm --force --recursive ~/.cache/gegl-0.3 ~/.config/GIMP \
+  rm --force --recursive ~/.cache/babl ~/.cache/gegl-0.3 ~/.config/GIMP \
       ~/.local/share/gegl-0.3
 
-  find ~ -iname "*gegl*" -or -iname "*gimp*" ! -path "${HOME}/.cargo/*" ! \
-      -path "${HOME}/.config/rc_files/*" ! -path "${HOME}/.vim/*"
+  find ~ -iname "*babl*" -or -iname "*gegl*" -or -iname "*gimp*" ! -path \
+      "${HOME}/.cargo/*" ! -path "${HOME}/.config/rc_files/*" ! -path \
+      "${HOME}/.vim/*"
 }
