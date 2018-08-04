8514caabe03a6230bcbcf212e78854a8b78e62f1
Mike Zueff <mike.zueff@gmail.com>
2018-08-04 20:14:16 +0300

    Update cleaners.

--- .config/rc_files/gimp.sh
+++ .config/rc_files/gimp.sh
@@ -1,7 +1,7 @@
 function _rc_gimp_clean_up
 {
-  rm --force --recursive ~/.cache/babl ~/.cache/gegl-0.3 ~/.config/GIMP \
-      ~/.local/share/gegl-0.3
+  rm --force --recursive ~/.cache/babl ~/.cache/gegl-0.4 ~/.config/GIMP \
+      ~/.local/share/gegl-0.4
 
   find ~ -iname "*babl*" -or -iname "*gegl*" -or -iname "*gimp*" ! -path \
       "${HOME}/.cargo/*" ! -path "${HOME}/.config/rc_files/*" ! -path \
--- .config/rc_files/vim.sh
+++ .config/rc_files/vim.sh
@@ -1,6 +1,7 @@
 function _rc_vim_reinstall
 {
-  rm --force --recursive ~/.cache/ctrlp ~/.cache/go-build ~/.cache/jedi ~/.npm \
-      ~/.pylint.d ~/.vim ~/.viminfo ~/.viminfo.tmp ~/.viminfz.tmp ~/.vimtags
+  rm --force --recursive ~/.cache/ctrlp ~/.cache/go-build ~/.cache/jedi \
+    ~/.mono ~/.npm ~/.pylint.d ~/.vim ~/.viminfo ~/.viminfo.tmp ~/.viminfz.tmp \
+    ~/.vimtags
   vim
 }
