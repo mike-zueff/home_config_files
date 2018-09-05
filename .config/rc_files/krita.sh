f81ddd99bc7e8a87aab6036f54f6daa2828c185f
Mike Zueff <mike.zueff@gmail.com>
2018-09-06 00:39:48 +0300

    Remove krita and wine cleaners

--- .config/rc_files/krita.sh
+++
@@ -1,7 +0,0 @@
-function _rc_krita_clean_up
-{
-  rm --force --recursive ~/.config/kritarc ~/.local/share/krita
-
-  find ~ -iname "*krita*" -not -path "${HOME}/.config/rc_files/*" -not -path \
-      "${HOME}/.vim/*"
-}
--- .config/rc_files/wine.sh
+++
@@ -1,8 +0,0 @@
-function _rc_wine_clean_up
-{
-  rm --force --recursive ~/.cache/winetricks ~/.wine
-
-  find ~ -iname "*wine*" -not -path "${HOME}/.cargo/*" -not -path \
-      "${HOME}/.config/rc_files/*" -not -path "${HOME}/.local/share/icons/*" \
-      -not -path "${HOME}/.local/share/mime/*" -not -path "${HOME}/.vim/*"
-}
