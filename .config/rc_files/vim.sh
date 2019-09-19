44f0f209314648b6005895d6a26883c308bb7268
Mike Zueff <mike.zueff@gmail.com>
2019-09-19 18:41:29 +0300

    Fix Vim upgrade script

--- .config/rc_files/vim.sh
+++ .config/rc_files/vim.sh
@@ -1,7 +1,7 @@
 function _rc_vim_reinstall
 {
   rm --force --recursive ~/.cache/ctrlp ~/.cache/go-build ~/.cache/jedi \
-    ~/.mono ~/.npm ~/.pylint.d ~/.vim ~/.viminfo ~/.viminfo.tmp ~/.viminfz.tmp \
-    ~/.vimtags
+      ~/.mono ~/.npm ~/.pylint.d ~/.vim ~/.viminfo ~/.viminfo.tmp \
+      ~/.viminfz.tmp ~/.vimtags ~/go
   vim
 }
