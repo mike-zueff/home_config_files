5447f67ff152e4288e9f98b77a2fa8ed76d6ca6d
Mike Zueff <mike.zueff@gmail.com>
2019-10-29 23:35:35 +0300

    Fix backup scripts

--- .config/rc_files/network.sh
+++ .config/rc_files/network.sh
@@ -3,18 +3,18 @@ function _rc_fix_network
   [[ `sudo whoami` != "root" ]] && return 1
 
   echo Flushing IPv6 addresses...
-  sleep 5
+  sleep 2
   sudo ip -6 address flush enp4s0
   echo Disabling NIC...
-  sleep 5
+  sleep 2
   sudo ifconfig enp4s0 down
-  sleep 5
+  sleep 2
   echo Enabling NIC...
   sudo ifconfig enp4s0 up
-  sleep 5
+  sleep 2
   echo Restarting services...
   sudo systemctl restart systemd-networkd.service systemd-resolved.service
-  sleep 5
+  sleep 2
   echo Done.
   sudo ifconfig
 }
