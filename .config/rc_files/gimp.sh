ded5bcc16be6de8c2b5744f8f64b57fd1a898121
Mike Zueff <mike.zueff@gmail.com>
2020-10-10 09:33:28 +0300

    Update GIMP and VIM scripts

--- .config/rc_files/gimp.sh
+++ .config/rc_files/gimp.sh
@@ -3,7 +3,7 @@ function _rc_gimp_clean_up
   rm --force --recursive ~/.cache/babl ~/.cache/gegl-0.4 ~/.cache/gimp \
       ~/.config/GIMP ~/.local/share/gegl-0.4
 
-  find ~ -iname "*babl*" -or -iname "*gegl*" -or -iname "*gimp*" ! -path \
+  find ~ \( -iname "*babl*" -or -iname "*gegl*" -or -iname "*gimp*" \) ! -path \
       "${HOME}/.cargo/*" ! -path "${HOME}/.config/rc_files/*" ! -path \
-      "${HOME}/.vim/*"
+      "${HOME}/.vim/*" ! -path "${HOME}/Nextcloud/*"
 }
--- .config/rc_files/vim.sh
+++ .config/rc_files/vim.sh
@@ -1,5 +1,6 @@
 function _rc_vim_reinstall
 {
+  chmod --recursive 777 ~/.vim 
   rm --force --recursive ~/.cache/ctrlp ~/.cache/go-build ~/.cache/jedi \
       ~/.mono ~/.pylint.d ~/.vim ~/.viminfo ~/.viminfo.tmp ~/.viminfz.tmp \
       ~/.vimtags ~/go
