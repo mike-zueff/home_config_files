6b7b9c7e22f1ed78fe26c2930ce627131cf6b3b1
Mike Zueff <mike.zueff@gmail.com>
2020-10-22 21:17:27 +0300

    Add MediaWiki reminder

---
+++ .config/awesome/mediawiki.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+INTERVAL_NOTIFICATION_S=$(( 60 * 60 ))
+INTERVAL_SLEEP_S=20
+TIME_START_S=$(date +%s)
+
+TIME_LAST_S=$(( TIME_START_S - TIME_START_S % INTERVAL_NOTIFICATION_S ))
+
+while true
+do
+  TIME_CURRENT_S=$(date +%s)
+  TIME_NEAREST_S=$(( TIME_LAST_S + INTERVAL_NOTIFICATION_S ))
+
+  if [[ TIME_CURRENT_S -gt TIME_NEAREST_S ]]
+  then
+    TIME_LAST_S=$TIME_NEAREST_S
+
+    INTERVAL_EXPIRATION_U=$(( (INTERVAL_NOTIFICATION_S + 2 * INTERVAL_SLEEP_S) * 1000 ))
+    /usr/bin/notify-send --expire-time $INTERVAL_EXPIRATION_U --icon /usr/share/icons/Tango/scalable/apps/internet-web-browser.svg "<b>MediaWiki</b><br />Please visit the private MediaWiki."
+  fi
+
+  sleep $INTERVAL_SLEEP_S
+done
