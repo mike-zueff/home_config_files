2f2019c52164556a106e916815c9afd57942e9f7
Mike Zueff <mike.zueff@gmail.com>
2022-05-12 11:13:12 +0300

    Update VPN config

--- .config/rc_files/vpn.sh
+++ .config/rc_files/vpn.sh
@@ -1,16 +1,33 @@
-function _rc_vpn_ssh_connect_default
+function _rc_vpn_gcorelabs_ssh_connect_default
 {
-  _rc_vpn_ssh_connect_ipv4
+  _rc_vpn_gcorelabs_ssh_connect_ipv4
 }
 
-function _rc_vpn_ssh_connect_ipv4
+function _rc_vpn_gcorelabs_ssh_connect_ipv4
 {
   . ~/.config/rc_files/credentials.sh
-  ssh -i /private/keys/scaleway/id_rsa root@"$_RC_TMP_VPN_IPV4"
+  sshpass -p"$_RC_TMP_GCORELABS_PASSWORD" ssh root@"$_RC_TMP_GCORELABS_VPN_IPV4"
 }
 
-function _rc_vpn_ssh_connect_ipv6
+function _rc_vpn_gcorelabs_ssh_connect_ipv6
 {
   . ~/.config/rc_files/credentials.sh
-  ssh -i /private/keys/scaleway/id_rsa root@"$_RC_TMP_VPN_IPV6"
+  sshpass -p"$_RC_TMP_GCORELABS_PASSWORD" ssh root@"$_RC_TMP_GCORELABS_VPN_IPV6"
+}
+
+function _rc_vpn_scaleway_ssh_connect_default
+{
+  _rc_vpn_scaleway_ssh_connect_ipv4
+}
+
+function _rc_vpn_scaleway_ssh_connect_ipv4
+{
+  . ~/.config/rc_files/credentials.sh
+  ssh -i /private/keys/scaleway/id_rsa root@"$_RC_TMP_SCALEWAY_VPN_IPV4"
+}
+
+function _rc_vpn_scaleway_ssh_connect_ipv6
+{
+  . ~/.config/rc_files/credentials.sh
+  ssh -i /private/keys/scaleway/id_rsa root@"$_RC_TMP_SCALEWAY_VPN_IPV6"
 }
