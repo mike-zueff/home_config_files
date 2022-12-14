7e3bb9d7a34f4875a75d875c3959ff21381b9fae
Mike Zueff <mike.zueff@gmail.com>
2022-12-14 12:05:52 +0300

    Fix path

--- .config/rc_files/eigengrau.sh
+++ .config/rc_files/eigengrau.sh
@@ -6,7 +6,7 @@ function _rc_create_backup_eigengrau
   if [[ ! -d ${_RC_EIGENGRAU_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP} ]]
   then
     mkdir "${_RC_EIGENGRAU_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP}"
-    tar --create --directory /private/projects --exclude eigengrau/.git eigengrau | xz --best > "${_RC_EIGENGRAU_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP}/eigengrau.tar.xz"
+    tar --create --directory /zfs --exclude eigengrau/.git eigengrau | xz --best > "${_RC_EIGENGRAU_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP}/eigengrau.tar.xz"
     _util_clear_old_backups ${_RC_EIGENGRAU_BACKUPS_DIRECTORY}
     mc ${_RC_EIGENGRAU_BACKUPS_DIRECTORY} "${_RC_EIGENGRAU_BACKUPS_DIRECTORY}/${_RC_CURRENT_DATE_STAMP}"
   fi
