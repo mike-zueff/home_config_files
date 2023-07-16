3a71f1ee77e01768cb04354d4b707debb70f6de8
Mike Zueff <mike.zueff@gmail.com>
2023-07-16 15:46:31 +0300

    Implement pipewire scripts

--- .config/rc_files/recordmydesktop.sh
+++ .config/rc_files/recordmydesktop.sh
@@ -1,3 +1,39 @@
+function _rc_parecord
+{
+  _RC_REC_DATE=$(date +%Y_%m_%d_%H_%M_%S)
+
+  _rc_pipewire_reset
+  _rc_pipewire_prepare
+  parecord --device desktop-record-virtual-microphone-1 "/zfs/rec_sound/${_RC_REC_DATE}.wav"
+}
+
+function _rc_pipewire_prepare
+{
+  pactl load-module module-null-sink \
+    channel_map=stereo \
+    media.class=Audio/Sink \
+    sink_name=desktop-record-combined-sink-1
+
+  pactl load-module module-null-sink \
+    channel_map=front-left,front-right \
+    media.class=Audio/Source/Virtual \
+    sink_name=desktop-record-virtual-microphone-1
+
+  pw-link alsa_output.pci-0000_07_00.0.analog-stereo:monitor_FL desktop-record-combined-sink-1:playback_FL
+  pw-link alsa_output.pci-0000_07_00.0.analog-stereo:monitor_FR desktop-record-combined-sink-1:playback_FR
+
+  pw-link alsa_input.pci-0000_00_1b.0.analog-stereo:capture_FL desktop-record-combined-sink-1:playback_FL
+  pw-link alsa_input.pci-0000_00_1b.0.analog-stereo:capture_FR desktop-record-combined-sink-1:playback_FR
+
+  pw-link desktop-record-combined-sink-1:monitor_FL desktop-record-virtual-microphone-1:input_FL
+  pw-link desktop-record-combined-sink-1:monitor_FR desktop-record-virtual-microphone-1:input_FR
+}
+
+function _rc_pipewire_reset
+{
+  pactl unload-module module-null-sink
+}
+
 function _rc_recordmydesktop
 {
   _RC_REC_COMMAND=""
