5dc5f32ec24675b5e9d7787151d7413a2416fae9
Mike Zueff <mike.zueff@gmail.com>
2024-06-18 16:05:20 +0300

    Update recorder

--- .config/rc_files/recorder.sh
+++ .config/rc_files/recorder.sh
@@ -58,3 +58,27 @@ function _rc_obsolete_recordmydesktop
   echo "${_RC_REC_COMMAND}"
   eval "${_RC_REC_COMMAND}"
 }
+
+function _rc_record_1_headphones
+{
+  local _RC_TMP_DEVICE=""
+
+  _RC_TMP_DEVICE=$(pw-cli ls | grep -B 100 -i alsa_output.pci-0000_07_00.0.analog-stereo | grep object.serial | tail -n 1 | sed 's/.*= //' | sed 's/"//g')
+  pw-cat --record --target "$_RC_TMP_DEVICE" 1_headphones.wav
+}
+
+function _rc_record_2_microphone
+{
+  pw-cat --record 2_microphone.wav
+}
+
+function _rc_record_3_desktop
+{
+  ffmpeg -video_size 1920x1200 -framerate 25 -f x11grab -i :0.0+0,1080 -y 3_desktop.mp4
+}
+
+function _rc_record_merge_all_files
+{
+  ffmpeg -i 1_headphones.wav -i 2_microphone.wav -filter_complex "[0][1]amerge=inputs=2,pan=stereo|FL<c0+c2|FR<c1+c3[a]" -map "[a]" -b:a 320k -y 4_merged_audio.mp3
+  ffmpeg -i 3_desktop.mp4 -i 4_merged_audio.mp3 -map 0:v -map 1:a -c copy -y 5_merged_video_and_audio.mp4
+}
