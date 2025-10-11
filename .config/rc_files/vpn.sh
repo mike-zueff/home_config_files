a87a0fc759942113526ef175c94238718a361652
Mike Zueff <mike.zueff@gmail.com>
2025-10-11 19:01:07 +0300

    Update vpn.sh

--- .config/rc_files/vpn.sh
+++ .config/rc_files/vpn.sh
@@ -14,3 +14,9 @@ function _rc_vpn_pq_ssh_connect_ipv6
   . ~/.config/rc_files/credentials.sh
   sshpass -p"$_RC_PQ_VPN_PASSWORD" ssh root@"$_RC_PQ_VPN_IPV6"
 }
+
+function _rc_vpn_ru_ssh_connect_default
+{
+  . ~/.config/rc_files/credentials.sh
+  sshpass -p"$_RC_RU_VPN_PASSWORD" ssh root@"$_RC_RU_VPN_IPV4"
+}
