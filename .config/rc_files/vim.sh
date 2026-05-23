ab984ba6ac4e12669a1465cc2cae6737e95e93ba
Mike Zueff <mike.zueff@gmail.com>
2026-05-23 23:11:49 +0300

    Update script

--- .config/rc_files/vim.sh
+++ .config/rc_files/vim.sh
@@ -1,8 +1,8 @@
 function _rc_vim_reinstall
 {
   chmod --recursive 777 ~/.vim || true
-  rm --force --recursive ~/.cache/ctrlp ~/.cache/go-build ~/.cache/jedi \
+  rm --force --recursive ~/.cache/ctrlp ~/.cache/jedi \
       ~/.mono ~/.pylint.d ~/.vim ~/.viminfo ~/.viminfo.tmp ~/.viminfz.tmp \
-      ~/.vimtags ~/go
+      ~/.vimtags
   vim
 }
