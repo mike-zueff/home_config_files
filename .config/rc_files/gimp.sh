49a13e631ec6cc34d709459739df23e088de7328
Mike Zueff <mike.zueff@gmail.com>
2026-06-12 19:18:49 +0300

    Fix rc_files

--- .config/rc_files/gimp.sh
+++ .config/rc_files/gimp.sh
@@ -1,9 +1,18 @@
 function _rc_gimp_clean_up
 {
-  rm --force --recursive ~/.cache/babl ~/.cache/gegl-0.4 ~/.cache/gimp \
-      ~/.config/GIMP ~/.local/share/gegl-0.4
+  rm --force --recursive \
+    ~/.cache/babl \
+    ~/.cache/gegl-0.4 \
+    ~/.cache/gimp \
+    ~/.config/GIMP \
+    ~/.local/share/gegl-0.4
 
-  find ~ \( -iname "*babl*" -or -iname "*gegl*" -or -iname "*gimp*" \) ! -path \
-      "${HOME}/.cargo/*" ! -path "${HOME}/.config/rc_files/*" ! -path \
-      "${HOME}/.vim/*" ! -path "${HOME}/Nextcloud/*"
+  find ~ \( -iname "*babl*" -or -iname "*gegl*" -or -iname "*gimp*" \) \
+    ! -path "${HOME}/.cargo/*" \
+    ! -path "${HOME}/.config/rc_files/*" \
+    ! -path "${HOME}/.vim/*" \
+    ! -path "${HOME}/Nextcloud/*" \
+    ! -path "${HOME}/go/*"
+
+  :
 }
